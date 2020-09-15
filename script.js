05a49f0ee0bc243f1d832506baef13a270c713f5
Roosevelt <panta_85.x@hotmail.com>
2020-09-15 08:28:45 -0500

    circles

--- script.js
+++ script.js
@@ -231,6 +231,28 @@ function rectangle(start, state, dispatch) {
   return drawRectangle;
 }
 
+function circle(pos, state, dispatch) {
+  function drawCircle(to) {
+    let radius = Math.sqrt(Math.pow(to.x - pos.x, 2) +
+                           Math.pow(to.y - pos.y, 2));
+    let radiusC = Math.ceil(radius);
+    let drawn = [];
+    for (let dy = -radiusC; dy <= radiusC; dy++) {
+      for (let dx = -radiusC; dx <= radiusC; dx++) {
+        let dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+        if (dist > radius) continue;
+        let y = pos.y + dy, x = pos.x + dx;
+        if (y < 0 || y >= state.picture.height ||
+            x < 0 || x >= state.picture.width) continue;
+        drawn.push({x, y, color: state.color});
+      }
+    }
+    dispatch({picture: state.picture.draw(drawn)});
+  }
+  drawCircle(pos);
+  return drawCircle;
+}
+
 const around = [
   { dx: -1, dy: 0 },
   { dx: 1, dy: 0 },
@@ -378,7 +400,7 @@ const startState = {
   doneAt: 0
 };
 
-const baseTools = {draw, fill, rectangle, pick};
+const baseTools = {draw, fill, rectangle, circle, pick};
 
 const baseControls = [
   ToolSelect, ColorSelect, SaveButton, LoadButton, UndoButton
